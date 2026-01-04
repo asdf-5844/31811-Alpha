ef8ecc7aa265b1d6996542eb9e2cc6f75fe4c412
beneben <bchen5844@gmail.com>
2026-01-04 13:43:08 -0500

    It is there but who knows if it works

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/AllianceMirror.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import com.pedropathing.geometry.Pose;
+
+public class AllianceMirror {
+    public static final double FIELD_SIZE = 144.0;
+
+    public static Pose mirrorPose(Pose p, boolean isRed) {
+        if (!isRed) return p;
+        return new Pose(
+                FIELD_SIZE - p.getX(),
+                p.getY()
+        );
+        // Pose in pedro is (x,y)
+        // Headings are handled separately by mirroring angles
+    }
+
+    public static double mirrorHeading(double headingRad, boolean isRed) {
+        if (!isRed) return headingRad;
+        return Math.toRadians(180)-headingRad; // The heading is mirrored
+    }
+}
+
