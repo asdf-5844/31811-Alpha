56638980e0e24873a91234272b721c432d101ced
beneben <bchen5844@gmail.com>
2026-02-04 17:10:05 -0500

    Disable 12

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/BlueClose12.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/BlueClose12.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.opmode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -14,6 +15,7 @@ import org.firstinspires.ftc.teamcode.subsystem.Intake;
 import org.firstinspires.ftc.teamcode.subsystem.Transport;
 import org.firstinspires.ftc.teamcode.subsystem.Outtake3;
 
+@Disabled
 @Autonomous(name = "BlueClose12", group = "Twelve")
 public class BlueClose12 extends LinearOpMode {
 
